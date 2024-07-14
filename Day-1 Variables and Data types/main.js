e6098ef13c1e07e958d896d68246cbf471d9beaf
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-07-14 09:42:06 +0500

    variables and data types complete

---
+++ Day-1 Variables and Data types/main.js
@@ -0,0 +1,51 @@
+// Day 1: Variables and Data Types
+
+// Activity 1: Variable Declaration
+// • Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+
+var myNumber = 123456789
+console.log(myNumber);
+
+// • Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+
+let MyString = 'haseeb ur rehman js'
+console.log(MyString);
+
+// Activity 2: Constant Declaration
+// • Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+
+const myBoolean = true
+console.log(myBoolean);
+
+// Activity 3: Data Types
+// Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type using the typeof operator.
+
+let userName = 'haseeb ur rehman js'
+let myPhoneNumber = 3167979839
+let userLoggedIn = true
+let employee = {
+    eployeeName :'jawad akhtair',
+    sallary : 500000,
+    id : 193056,
+} 
+let myArray = ['haseeb', 'jawad', 'khalid', 'ahmed']
+
+console.log(typeof userName);
+console.log(typeof myNumber);
+console.log(typeof userLoggedIn);
+console.log(typeof employee);
+console.log(typeof myArray);
+
+// Activity 4: Reassigning Variables
+// Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console.
+
+console.log(userName);
+userName = 'Muhammad Rohan'
+console.log(userName);
+
+// Activity 5: Understanding const
+
+const MyName = 'rohan'
+console.log(MyName);
+MyName = 'set abid' // ===> this is constaint variable
+console.log(MyName);
